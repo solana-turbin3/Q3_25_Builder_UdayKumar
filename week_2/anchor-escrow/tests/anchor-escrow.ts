d4661263590b3735249787eeed5b9a25f3d4e4c1
Uday Kumar <kuday0726@gmail.com>
2025-08-03 18:00:05 +0000

    added changes

--- week_2/anchor-escrow/tests/anchor-escrow.ts
+++ week_2/anchor-escrow/tests/anchor-escrow.ts
@@ -2,6 +2,8 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorEscrow } from "../target/types/anchor_escrow";
 
+
+
 describe("anchor-escrow", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
